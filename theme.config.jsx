a5f3a0c6e8edcf704831b0888e53cda037a6eb8b
Yoshihiro Koyama <h2so4@mineo.jp>
2024-02-04 22:44:17 +0900

    クローラー除けのおまじない

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,7 @@ export default {
   logo: <span>My Nextra Documentation</span>,
   project: {
     link: 'https://github.com/shuding/nextra'
-  }
+  },
+  head: <meta name="robots" content="none" />,
   // ... other theme options
 }
