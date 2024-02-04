88d482aaad00f87a0dc5b529598a1ec2cbb38231
Yoshihiro Koyama <h2so4@mineo.jp>
2024-02-04 23:10:52 +0900

    fixed theme

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,17 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra'
-  },
+  logo: <span>バイオインフォマティクス講習会</span>,
   head: <meta name="robots" content="none" />,
+  project: {
+    link: 'https://github.com/hirosuke29/bioinformatics-starter'
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
+  footer: {
+    text: 'CC BY-ND 4.0',
+  }
   // ... other theme options
 }
